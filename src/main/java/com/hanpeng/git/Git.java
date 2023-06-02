cd8b6a20d0844f1e018bb3b659e783edc842e9a6
hanpeng <1635520538@qq.com>
2023-06-02 23:29:11 +0800

    second commit

--- src/main/java/com/hanpeng/git/Git.java
+++ src/main/java/com/hanpeng/git/Git.java
@@ -2,6 +2,6 @@ package com.hanpeng.git;
 
 public class Git {
     public static void main(String[] args) {
-        System.out.println("hello git ........");
+        System.out.println("hello git 2.0........");
     }
 }

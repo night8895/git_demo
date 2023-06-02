564a703b18883c45f730433adff1c68ccadcbb72
hanpeng <1635520538@qq.com>
2023-06-02 23:42:10 +0800

    hot-fix commit1

--- src/main/java/com/hanpeng/git/Git.java
+++ src/main/java/com/hanpeng/git/Git.java
@@ -3,5 +3,6 @@ package com.hanpeng.git;
 public class Git {
     public static void main(String[] args) {
         System.out.println("hello git 3.0........");
+        System.out.println("hello git 4.0........");
     }
 }
